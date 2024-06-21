56bbe52bd3c5feeb36521cb028c0ad4e53de9a93
ccbikai <ccbikai@gmail.com>
2024-06-21 20:49:27 +0800

    chore: Add CloudflareEnv interface for DB binding

--- shims.d.ts
+++ shims.d.ts
@@ -39,3 +39,11 @@ interface Window {
     }
   }
 }
+interface CloudflareEnv {
+  // The DB Namespace binding type used here comes
+  // from `@cloudflare/workers-types`. To use it in such
+  // a way make sure that you have installed the package
+  // as a dev dependency and you have added it to your
+  // `tsconfig.json` file under `compilerOptions.types`.
+  DB: D1Database
+}
